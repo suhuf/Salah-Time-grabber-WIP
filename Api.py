834ef000c46306227bcce263d297080567db1bde
suhuf <105312929+suhuf@users.noreply.github.com>
2023-09-13 06:35:33 -0400

    Mistakenly added additions

--- Api.py
+++ Api.py
@@ -1,41 +1,5 @@
 import requests
 import json
-import customtkinter
-
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("blue")
-
-root = customtkinter.CTk()
-
-root.geometry("500x350")
-
-def login():
-    print("Test")
-
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
-
-label = customtkinter.CTkLabel(master=frame, text="Login System", font=("Roboto", 24))
-
-label.pack(pady=12, padx=10)
-
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="???")
-
-entry1.pack(pady=12, padx=10)
-
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="?????", show="*")
-entry2.pack(pady=12, padx=10)
-
-button = customtkinter.CTkButton(master=frame, text="Login", command=login)
-
-button.pack(pady=12, padx=10)
-
-root.mainloop()
-
-
-
-
-
 
 url = 'http://www.islamicfinder.us/index.php/api/prayer_times'
 
