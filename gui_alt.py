b2b5b3bcca20df506b4feb497fb12a292c3fe8aa
suhuf <105312929+suhuf@users.noreply.github.com>
2023-09-21 09:54:42 -0400

    Update gui_alt.py
    Made Gui more functional with QoL Input field. 
    
    Next step is adding GUI functionality for State and other parameters

--- gui_alt.py
+++ gui_alt.py
@@ -4,85 +4,121 @@ import json
 import tkinter as tk
 
 from tkinter import messagebox
+from tkinter import *
 
-from tkinter import simpledialog
+import tkinter.simpledialog as sd
 
 url = 'http://www.islamicfinder.us/index.php/api/prayer_times'
 
-class Gui:
-    def __init__(self):
 
-        self.root = tk.Tk()
 
-        self.label = tk.Label(self.root, text="Message", font=('Arial'))
+def on_entry_click(event):
+   if textbox.get() == "Enter your zipcode":
+      textbox.delete(0, tk.END)
+      textbox.configure(foreground="black")
 
-        self.label.pack(padx=10, pady=10)
+def on_focus_out(event):
+   if textbox.get() == "":
+      textbox.insert(0, "Enter your zipcode")
+      textbox.configure(foreground="gray")
 
-        self.textbox = tk.Text(self.root, height=5, font=('Arial', 16))
 
-        self.textbox.bind("<KeyPress>", self.sc)
 
-        self.textbox.pack(padx=10, pady=10)
+def user_info():
+    zipcode = sd.askstring("Provide zipcode")
+    
+    
 
-        self.button = tk.Button(self.root, text="Send", font=('Arial', 18), command=self.api_call)
+def testing():
 
-        self.button.pack(padx=10, pady=10)
+    dragon = textbox.get()
+    print(dragon)
 
-        self.root.mainloop()
-
-    def api_call(self):
+def api_call():
 
     
-        global entry
-        z = 20874
-        c = 'US'
+    global entry
 
-        a = requests.post(url, data={'zipcode': z, 'country': c })
+    textbox.get()
 
-        print("        ")
+    z = textbox.get()
+    c = 'US'
 
-        print("Response Code: " + str(a.status_code))
+    a = requests.post(url, data={'zipcode': z, 'country': c })
 
-        print("        ")
+    print("        ")
+
+    print("Response Code: " + str(a.status_code))
+
+    print("        ")
     
-        api_data = a.json()
+    api_data = a.json()
 
-        a_dict = a.json()
+    a_dict = a.json()
 
-        f_key = str(api_data['results']['Fajr'])
-        d_key = str(api_data['results']['Dhuhr'])
-        a_key = str(api_data['results']['Asr'])
-        m_key = str(api_data['results']['Maghrib'])
-        i_key = str(api_data['results']['Isha'])
+    f_key = str(api_data['results']['Fajr'])
+    d_key = str(api_data['results']['Dhuhr'])
+    a_key = str(api_data['results']['Asr'])
+    m_key = str(api_data['results']['Maghrib'])
+    i_key = str(api_data['results']['Isha'])
 
-        dict_keys = [f_key, d_key, a_key, m_key, i_key]
+    dict_keys = [f_key, d_key, a_key, m_key, i_key]
 
-        for j,i in enumerate(dict_keys):
-            if "%" in i:
-                dict_keys[j] = i.replace("%", " ")
+    for j,i in enumerate(dict_keys):
+        if "%" in i:
+            dict_keys[j] = i.replace("%", " ")
 
-        t_fajr = str(dict_keys[0])
-        t_dhur = str(dict_keys[1])
-        t_asr = str(dict_keys[2])
-        t_maghrib = str(dict_keys[3])
-        t_isha = str(dict_keys[4])
+    t_fajr = str(dict_keys[0])
+    t_dhur = str(dict_keys[1])
+    t_asr = str(dict_keys[2])
+    t_maghrib = str(dict_keys[3])
+    t_isha = str(dict_keys[4])
 
-        """ print("Fajr Time: " + t_fajr)
+    """ print("Fajr Time: " + t_fajr)
         print("Dhur Time: " + t_dhur)
         print("Asr Time: " + t_asr)
         print("Maghrib Time: " + t_maghrib)
-        print("Isha Time: " + t_isha) """
+    print("Isha Time: " + t_isha) """
 
-        messagebox.showinfo(title="Message", message="Response Code: " + str(a.status_code) + " Fajr Time: " + str(t_fajr)
-        + "Dhur Time: " + str(t_dhur)
-        + "Asr Time: " + str(t_asr)
-        + "Maghrib Time: " + str(t_maghrib) +
-        "Isha Time: " + str(t_isha) )
+    messagebox.showinfo(title="Message", message="Response Code: " + str(a.status_code) + " Fajr Time: " + str(t_fajr)
+    + "Dhur Time: " + str(t_dhur)
+    + "Asr Time: " + str(t_asr)
+    + "Maghrib Time: " + str(t_maghrib) +
+    "Isha Time: " + str(t_isha) )
         
-        pass
+    pass
 
-    def sc(self, event):
+def sc():
         
-        pass
+    pass
 
-Gui()
+root = tk.Tk()
+
+mystr = StringVar()
+mystr.set('enter zippy')
+
+label = tk.Label(root, text="Message", font=('Arial'))
+
+label.pack(padx=10, pady=10)
+
+textbox = Entry(root, width=50, font=('Arial', 16))
+
+textbox.insert(0, "Enter your zipcode")
+
+textbox.bind("<FocusIn>", on_entry_click)
+textbox.bind("<FocusOut>", on_focus_out)
+
+textbox.pack(padx=30, pady=30)
+
+
+
+
+textbox.get
+
+button = tk.Button(root, text="Send", font=('Arial', 18), command= api_call)
+
+button.pack(padx=10, pady=10)
+
+
+
+root.mainloop()
